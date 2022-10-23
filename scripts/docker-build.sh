d925205edfed0b4eab38d26a67024c30155af470
Oliver Alonso <alonso.oliverio@gmail.com>
2022-10-23 15:22:23 +0200

    add docker

---
+++ scripts/docker-build.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+docker build -t frontend-challenge-github-react:dev .
---
+++ scripts/docker-run.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+docker run -it --rm \
+-v ${PWD}:/app \
+-v /app/node_modules \
+-p 3001:3000 \
+-e CHOKIDAR_USEPOLLING=true \
+frontend-challenge-github-react:dev  
