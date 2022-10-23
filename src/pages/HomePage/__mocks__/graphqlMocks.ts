9e13cd7d1b5eee7de67ab46dc8633203ffaa9ad9
Oliver Alonso <alonso.oliverio@gmail.com>
2022-10-23 15:23:16 +0200

    add home page tests

---
+++ src/pages/HomePage/__mocks__/graphqlMocks.ts
@@ -0,0 +1,33 @@
+import { SEARCH_REPOSITORIES_PAGINATED } from "../../../infrastructure/github/queries";
+
+import searchResultsReactMock from "./searchResultsReact.json";
+import searchResultsReduxMock from "./searchResultsRedux.json";
+
+const searchMocks = [
+	{
+		request: {
+			query: SEARCH_REPOSITORIES_PAGINATED,
+			variables: {
+				query: "react",
+				first: 10,
+			},
+		},
+		result: {
+			data: searchResultsReactMock,
+		},
+	},
+	{
+		request: {
+			query: SEARCH_REPOSITORIES_PAGINATED,
+			variables: {
+				query: "redux",
+				first: 10,
+			},
+		},
+		result: {
+			data: searchResultsReduxMock,
+		},
+	},
+];
+
+export default searchMocks;
