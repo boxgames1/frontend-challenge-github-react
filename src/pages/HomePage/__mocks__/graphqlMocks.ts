51184aa3833a37f65456a848a90749569d9c742e
Oliver Alonso <alonso.oliverio@gmail.com>
2022-10-27 14:31:01 +0200

    update mocks

--- src/pages/HomePage/__mocks__/graphqlMocks.ts
+++ src/pages/HomePage/__mocks__/graphqlMocks.ts
@@ -1,7 +1,6 @@
 import { SEARCH_REPOSITORIES_PAGINATED } from '../../../infrastructure/github/queries';
 
 import searchResultsReactMock from './searchResultsReact.json';
-import searchResultsReact5Mock from './searchResultsReact5.json';
 import searchResultsReactPage2Mock from './searchResultsReactPage2.json';
 import searchResultsReduxMock from './searchResultsRedux.json';
 
@@ -16,18 +15,6 @@ const searchMocks = [
     },
     result: {
       data: searchResultsReactMock,
-    },
-  },
-  {
-    request: {
-      query: SEARCH_REPOSITORIES_PAGINATED,
-      variables: {
-        query: 'react',
-        first: 5,
-      },
-    },
-    result: {
-      data: searchResultsReact5Mock,
     },
   },
   {
