bd993e33573b2b231fe2e822f89b038e7cf9e559
Oliver Alonso <alonso.oliverio@gmail.com>
2022-10-27 12:27:14 +0200

    fix ts

--- src/gql/gql.ts
+++ src/gql/gql.ts
@@ -2,7 +2,7 @@
 import * as types from './graphql';
 import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
 
-const documents = [];
+const documents: any[] = [];
 export function graphql(source: string): unknown;
 export function graphql(source: string) {
   return (documents as any)[source] ?? {};
